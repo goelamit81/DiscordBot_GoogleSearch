b66cfc4388b076540b13ab79f498c997b34ab6f5
Amit Goel <amit.h2002468@gmail.com>
2020-04-10 23:00:49 +0530

    Add files via upload

--- bluestacks/environment.py
+++ bluestacks/environment.py
@@ -4,11 +4,12 @@
 GOOGLE_SEARCH_ENGINE_ID = 'GOOGLE_SEARCH_ENGINE_ID'
 GOOGLE_SEARCH_API_KEY = 'GOOGLE_SEARCH_API_KEY'
 
-# mysql db credentials
-MYSQL_DB_HOST = 'MYSQL_DB_HOST'
-MYSQL_DB_USER = 'MYSQL_DB_USER'
-MYSQL_DB_PASSWORD = 'MYSQL_DB_PASSWORD'
-MYSQL_DB_NAME = 'MYSQL_DB_NAME'
+# postgre db credentials
+PSQL_DB_HOST = 'PSQL_DB_HOST'
+PSQL_DB_PORT = PSQL_DB_PORT
+PSQL_DB_USER = 'PSQL_DB_USER'
+PSQL_DB_PASSWORD = 'PSQL_DB_PASSWORD'
+PSQL_DB_NAME='PSQL_DB_NAME'
 
 # discord credentials
 DISCORD_TOKEN = 'DISCORD_TOKEN'
@@ -20,12 +21,12 @@ SEND_GREETING = 'Hey'
 # result count
 MAX_SEARCH_RESULT_COUNT = 5
 
-# search keyword max length as mysql search key column length which is 255
-SEARCH_KEY_MAX_LENGTH = 255
+# search keyword max length as postgre sql search key column length which is 300
+SEARCH_KEY_MAX_LENGTH = 300
 
 # messages
 INPUT_REQUIRED_MSG = 'Please enter required input'
 NO_SEARCH_RESULT_MSG = 'No result found. Please try some other search'
-MAX_INPUT_LENGTH_MSG = 'Input should be less than 255 characters'
+MAX_INPUT_LENGTH_MSG = 'Input should be less than or equal to 300 characters'
 
 ################################################################################################
